05fafc16b33701064ca3ae40acb09c68ddd23faa
s2018w09 <s2018w09@e-mirim.hs.kr>
2020-07-03 15:32:29 +0900

    order/ models Order

--- order/models.py
+++ order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from menu.models import Drink
+
+
+class Order(models.Model):
+    #drink, stock, cup, ice, sugar, pearl, price
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE, related_name='order')
+    stock = models.PositiveIntegerField(default=1)
+    cup = models.IntegerField() #0: 레귤러, 1: 점보
+    ice = models.IntegerField(default=100) #0, 50, 100, 150%
+    sugar = models.IntegerField(default=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f'음료: {self.drink}, 개수 : {self.stock}, 컵사이즈 : {self.cup}, 얼음량 : {self.ice}, 당도 : {self.sugar}, 가격 : {self.price}'
